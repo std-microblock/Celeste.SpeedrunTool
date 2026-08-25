std-microblock/Celeste.SpeedrunTool

commit f062d6997279e8a4a63bd8479ee74807cbd9d13b

popup message when auto clear state on screen transition

SpeedrunTool/Source/SaveLoad/StateManager.cs
@@ -178,6 +178,9 @@ public sealed class StateManager {
             return;
         }
 
+        // v3.28 新功能: 切换 Scene 时不再自动删除 SaveState.
+        // 由于我们还保存了 Celeste.SaveData.Instance, 因此我们的多存档甚至可以存储 不同的原版存档文件上的不同章节
+
         if (ModSettings.AutoClearStateOnSceneSwitch) {
             if (self is Overworld) {
                 if (!SavedByTas && InGameOverworldHelperIsOpen.Value?.GetValue(null) as bool? != true) {
@@ -423,43 +426,25 @@ public sealed class StateManager {
         }
     }
 
-    // 释放资源，停止正在播放的声音等
-    private void UnloadLevel(Level level) {
-        List<Entity> entities = new();
+    // 触发 Entity.Removed, 从而释放资源，停止正在播放的声音, 解除 hook 等
+    private static void UnloadLevel(Level level) {
+        List<Entity> entities =
+        [
+            // Player 必须最早移除，不然在处理 player.triggersInside 字段时会产生空指针异常
+            .. level.Tracker.GetEntities<Player>(),
+            .. level.Entities,
+        ];
 
-        // Player 必须最早移除，不然在处理 player.triggersInside 字段时会产生空指针异常
-        entities.AddRange(level.Tracker.GetEntities<Player>());
-
-        // 移除当前房间的实体，照抄 level.UnloadLevel() 方法，不直接调用是因为 BingUI 在该方法中将其存储的 level 设置为了 null
-        AddNonGlobalEntities(level, entities);
-
-        // 恢復主音乐
-        if (level.Tracker.GetEntity<CassetteBlockManager>() is { } cassetteBlockManager) {
-            entities.Add(cassetteBlockManager);
-        }
+        // 等效于 Level.UnloadEntities 然后 UpdateLists
 
         foreach (Entity entity in entities.Distinct()) {
             try {
-                entity.Removed(level);
+                entity.Removed(level); // 触发各式各样的副作用
+                // Scene.TagLists / Scene.Tracker / Engine.Pooler 本身都将被克隆出来, 无需处理. 同理也不用 UpdateLists
             }
             catch (NullReferenceException) {
                 // ignore https://discord.com/channels/403698615446536203/954507384183738438/954507384183738438
             }
-        }
-
-        // 移除剩下声音组件
-        level.Tracker.GetComponentsCopy<SoundSource>().ForEach(component => component.RemoveSelf());
-    }
-
-    private void AddNonGlobalEntities(Level level, List<Entity> entities) {
-        int global = (int)Tags.Global;
-        foreach (Entity entity in level.Entities) {
-            if ((entity.tag & global) == 0) {
-                entities.Add(entity);
-                continue;
-            }
-
-            SaveLoadAction.OnUnloadLevel(level, entities, entity);
         }
     }
 
@@ -541,7 +526,7 @@ public sealed class StateManager {
 
     // 分两步的原因是更早的停止音乐，听起来更舒服更好一点
     // 第一步：停止播放主音乐
-    private void RestoreCassetteBlockManager1(Level level) {
+    private static void RestoreCassetteBlockManager1(Level level) {
         if (level.Tracker.GetEntity<CassetteBlockManager>() is { } manager) {
             manager.snapshot?.start();
         }
@@ -550,7 +535,7 @@ public sealed class StateManager {
     // 第二步：播放节奏音乐
     // https://discord.com/channels/403698615446536203/429775260108324865/1422017531098566776
     // 理论上, 在 Cassette cycle 图中 LoadState 后暂停会引入额外的读图优势, 但这显然不是我们的问题.
-    private void RestoreCassetteBlockManager2(Level level) {
+    private static void RestoreCassetteBlockManager2(Level level) {
         if (level.Tracker.GetEntity<CassetteBlockManager>() is { } manager) {
             if (manager.sfx is { } sfx && !manager.isLevelMusic && manager.leadBeats <= 0) {
                 sfx.start();
@@ -558,7 +543,7 @@ public sealed class StateManager {
         }
     }
 
-    internal void ClearStateImpl(bool hasGc = true) {
+    internal bool ClearStateImpl(bool hasGc = true) {
         // TODO: 这里 Task.Wait() 可能可以试着让它更快结束?
 
         preCloneTask?.Wait();
@@ -591,10 +576,7 @@ public sealed class StateManager {
             Logger.Info("SpeedrunTool/ClearState", $"Clear {FullSlotDescription}");
         }
         SlotDescription = "";
-    }
-
-    public void ClearStateAndShowMessage() {
-        ClearStateImpl(hasGc: true);
+        return doSomething;
     }
 
     private void PreCloneSavedEntities() {

SpeedrunTool/Source/SpeedrunToolModule.cs
@@ -34,6 +34,11 @@ public class SpeedrunToolModule : EverestModule {
     public override Type SaveDataType => typeof(SpeedrunToolSaveData);
 
     public override void Load() {
+#if RELEASE
+        string todo = "Boss Desync / ModSupportNotWell (should save whether hook is applied when saving and then when loading update the hook state to right status)";
+        throw new NotImplementedException(todo);
+#endif
+
         SaveSlotsManager.SwitchSlot(1); // i don't want to do a bunch of nullity checks
         StateManager.Load();
         AttributeUtils.Invoke<LoadAttribute>();

SpeedrunTool/Source/SaveLoad/Implements/DeepClonerUtils.cs
@@ -230,7 +230,7 @@ public static class DeepClonerUtils {
                 }
                 else if (type.IsIDictionary(out Type dictKeyType, out Type _) && !dictKeyType.IsSimple() &&
                            clonedObj is IDictionary { Count: > 0 } clonedDict) {
-                    backupDict ??= new Dictionary<object, object>();
+                    backupDict ??= [];
                     backupDict.SetRange(clonedDict);
                     clonedDict.Clear();
                     clonedDict.SetRange(backupDict);

SpeedrunTool/Source/SaveLoad/SaveLoadAction.cs
@@ -65,7 +65,6 @@ public sealed class SaveLoadAction {
     private readonly Action preCloneEntities;
     internal readonly SlAction loadState;
     private readonly SlAction saveState;
-    internal Action<Level, List<Entity>, Entity> unloadLevel;
     private int executeOrder;
 
     private static int? contextOrder = null;
@@ -92,18 +91,6 @@ public sealed class SaveLoadAction {
         this.beforeSaveState = beforeSaveState;
         this.beforeLoadState = beforeLoadState;
         this.preCloneEntities = preCloneEntities;
-        needInitializeDictionaryId = true;
-    }
-
-    public SaveLoadAction(SlAction saveState, SlAction loadState, Action clearState,
-        Action<Level> beforeSaveState, Action<Level> beforeLoadState, Action preCloneEntities, Action<Level, List<Entity>, Entity> unloadLevel) {
-        this.saveState = saveState;
-        this.loadState = loadState;
-        this.clearState = clearState;
-        this.beforeSaveState = beforeSaveState;
-        this.beforeLoadState = beforeLoadState;
-        this.preCloneEntities = preCloneEntities;
-        this.unloadLevel = unloadLevel;
         needInitializeDictionaryId = true;
     }
 
@@ -245,12 +232,6 @@ public sealed class SaveLoadAction {
     internal static void OnPreCloneEntities() {
         foreach (SaveLoadAction saveLoadAction in SharedActions) {
             saveLoadAction.preCloneEntities?.Invoke();
-        }
-    }
-
-    internal static void OnUnloadLevel(Level level, List<Entity> entities, Entity entity) {
-        foreach (SaveLoadAction saveLoadAction in SharedActions) {
-            saveLoadAction.unloadLevel?.Invoke(level, entities, entity);
         }
     }
 

SpeedrunTool/Source/SaveLoad/SaveSlotsManager.cs
@@ -113,8 +113,8 @@ internal static class SaveSlotsManager {
     /// <summary>
     /// Clear current save slot 
     /// </summary>
-    public static void ClearState() {
-        StateManagerInstance.ClearStateImpl(hasGc: true);
+    public static bool ClearState() {
+        return StateManagerInstance.ClearStateImpl(hasGc: true);
     }
 
     public static void ClearAll() {

SpeedrunTool/Source/SaveLoad/ThirdPartySupport/SpirialisHelperUtils.cs
@@ -35,12 +35,6 @@ internal static class SpirialisHelperUtils {
                     }
                 }
             );
-
-            action.unloadLevel = (_, entities, entity) => {
-                if (entity.GetType() == timeControllerType) {
-                    entities.Add(entity);
-                }
-            };
         }
 
         if (ModUtils.GetType("SpirialisHelper", "Celeste.Mod.Spirialis.TimeZipMover") is { } timeZipMoverType) {

SpeedrunTool/Source/SaveLoad/ThirdPartySupport/SpringCollab2020Utils.cs
@@ -3,8 +3,6 @@ using Celeste.Mod.SpeedrunTool.Utils;
 
 namespace Celeste.Mod.SpeedrunTool.SaveLoad.ThirdPartySupport;
 internal static class SpringCollab2020Utils {
-
-
     internal static void Support() {
         if (ModUtils.GetType("SpringCollab2020", "Celeste.Mod.SpringCollab2020.Entities.RainbowSpinnerColorController") is { } colorControllerType
             && colorControllerType.GetFieldInfo("colorControllerType") != null

SpeedrunTool/Source/SaveLoad/Utils/AutoClearStateOnScreenTrantition.cs
@@ -1,6 +1,6 @@
 namespace Celeste.Mod.SpeedrunTool.SaveLoad.Utils;
 
-internal static class AutoClearState {
+internal static class AutoClearStateOnScreenTrantition {
     [Load]
     private static void Load() {
         On.Celeste.Player.OnTransition += PlayerOnOnTransition;
@@ -19,7 +19,13 @@ internal static class AutoClearState {
             && !StateManager.Instance.SavedByTas
             && self.Scene is Level
            ) {
-            StateManager.Instance.ClearStateAndShowMessage();
+            bool b = SaveSlotsManager.ClearState();
+            // 只清除当前槽位, 不清除所有
+
+            if (b) {
+                Message.PopupMessageUtils.Show("Auto Clear State on Screen Transition", null);
+            }
+
         }
     }
 }

commit b023fdd6337354d51f47498b3401074b64e81a1b

fix bug caused by f062d699

SpeedrunTool/Source/SaveLoad/StateManager.cs
@@ -1,4 +1,4 @@
-using Celeste.Mod.SpeedrunTool.ModInterop;
+﻿using Celeste.Mod.SpeedrunTool.ModInterop;
 using Celeste.Mod.SpeedrunTool.Utils;
 using Force.DeepCloner;
 using Force.DeepCloner.Helpers;
@@ -403,6 +403,12 @@ public sealed class StateManager {
         // 等效于 Level.UnloadEntities 然后 UpdateLists
 
         foreach (Entity entity in entities.Distinct()) {
+            // TODO: 确定到底哪些 Global entity 可以移除
+            // 这里知道的是 MirrorSurfaces 被移除会导致一些 VirtualRenderTarget Dispose
+            // 于是如果在存档 1 存档, 在存档 2 清除, 再读档 1, 会崩溃
+            if (entity.TagCheck(Tags.Global) && !GlobalEntitiesToRemove.Contains(entity.GetType())) {
+                continue;
+            }
             try {
                 entity.Removed(level); // 触发各式各样的副作用
                 // Scene.TagLists / Scene.Tracker / Engine.Pooler 本身都将被克隆出来, 大部分情况下无需处理. 同理也不用 UpdateLists
@@ -415,6 +421,18 @@ public sealed class StateManager {
             catch (NullReferenceException) {
                 // ignore https://discord.com/channels/403698615446536203/954507384183738438/954507384183738438
             }
+        }
+
+        // 移除剩下声音组件
+        level.Tracker.GetComponentsCopy<SoundSource>().ForEach(component => component.RemoveSelf());
+    }
+
+    private static readonly HashSet<Type> GlobalEntitiesToRemove = [typeof(CassetteBlockManager)];
+
+    [Initialize]
+    private static void InitGlobalEntitiesToRemove() {
+        if (ModUtils.GetType("SpirialisHelper", "Celeste.Mod.Spirialis.TimeController") is { } timeControllerType) {
+            GlobalEntitiesToRemove.Add(timeControllerType);
         }
     }
 

SpeedrunTool/Source/SpeedrunToolModule.cs
@@ -1,4 +1,4 @@
-using Celeste.Mod.SpeedrunTool.SaveLoad;
+﻿using Celeste.Mod.SpeedrunTool.SaveLoad;
 using FMOD.Studio;
 
 namespace Celeste.Mod.SpeedrunTool;
@@ -49,7 +49,8 @@ public class SpeedrunToolModule : EverestModule {
             "/ https://discord.com/channels/403698615446536203/1523162448675340369" +
             "/ https://discord.com/channels/403698615446536203/1145455251495845949/1525818371998748783" +
             "/ SSC4 中 Aqua mod 开启似乎就会导致 SL 很卡, 不过仅限于前几次? 但是不开 Aqua 就没事" +
-            "/ 似乎初次 SL 之后直接 ClearState, 再 SL 就不怎么卡? 以及 lag spike 似乎也会减少";
+            "/ 似乎初次 SL 之后直接 ClearState, 再 SL 就不怎么卡? 以及 lag spike 似乎也会减少" +
+            "/ 可能考虑重写 StateManager.UnloadLevel(), 想办法直接避免触发一些 hook 卸载/加载的副作用?";
 
         throw new NotImplementedException(todo);
 #endif